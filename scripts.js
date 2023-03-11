4bd4b5e1e2475958a158ae12eb4b152aa26949a1
indirectsleep <111393037+indirectsleep@users.noreply.github.com>
2023-03-11 23:26:19 +0200

    1.04

--- scripts.js
+++ scripts.js
@@ -5,3 +5,7 @@ function zoomIn(img_src){
 function zoomVideo(vid_src){
     document.getElementById("modalVideo").src=vid_src
 }
+
+function zoomTest(test_src){
+    document.getElementById("testImg").src=test_src
+}
