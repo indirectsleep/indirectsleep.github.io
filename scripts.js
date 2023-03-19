220f0211bd3f311a71a886ac1498d92e0687627d
indirectsleep <111393037+indirectsleep@users.noreply.github.com>
2023-03-19 18:39:42 +0200

    1.061

--- scripts.js
+++ scripts.js
@@ -6,8 +6,9 @@ function zoomVideo(vid_src){
     document.getElementById("modalVideo").src=vid_src
 }
 
-function zoomTest(test_src){
+function zoomTest(test_src,test_title){
     document.getElementById("testImg").src=test_src
+    document.getElementById("testTitle").innerHTML = test_title
 }
 
 function stopVideo(){
