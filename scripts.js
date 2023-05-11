5324c14904f7f451ae6308ed2969f6bae40c3601
indirectsleep <111393037+indirectsleep@users.noreply.github.com>
2023-05-11 23:23:10 +0300

    1.11
    buttons

--- scripts.js
+++ scripts.js
@@ -1,19 +1,22 @@
-function zoomIn(img_src){
-    document.getElementById("modalImage").src=img_src
-}
-
-function zoomVideo(vid_src){
-    document.getElementById("modalVideo").src=vid_src
-}
-
 function zoomTest(test_src,test_title,test_id){
+    document.getElementById("prev").disabled = false;
+    document.getElementById("nxt").disabled = false;
+
     document.getElementById("testImg").src=test_src
     document.getElementById("testTitle").innerHTML = test_title
 
     var num = test_id.match(/\d/g);
     num = num.join("");
+    document.getElementById("testIndex").innerHTML = num
 
-    document.getElementById("testValue").value = num
+    images = document.getElementsByClassName("col");
+    c = images.length;
+    document.getElementById("numbtxt").innerHTML = num + " / " + c
+
+}
+
+function zoomVideo(vid_src){
+    document.getElementById("modalVideo").src=vid_src
 }
 
 function stopVideo(){
@@ -23,8 +26,31 @@ function stopVideo(){
 }
 
 
-// 1. change src of testImg
+  function plusImages(n){
+    images = document.getElementsByClassName("col");
+    c = images.length;
 
-//  - get id of testImg
-//  - get src of element with id of id+1
-//  - testImg.src = new source
+    currentSrc = document.getElementById("testImg").src;
+    currentTit = document.getElementById("testImg").title;
+    currentIndex = document.getElementById("testIndex").innerHTML;
+
+    i = Number(currentIndex);
+    i += n;
+
+    if (i >= c) {document.getElementById("nxt").disabled = true}
+    if (i <= 1) {document.getElementById("prev").disabled = true}
+    if (i > 1) {document.getElementById("prev").disabled = false}
+    if (i < c) {document.getElementById("nxt").disabled = false}
+
+    nextSrc = document.getElementById(String("img"+i)).src;
+    nextTit = document.getElementById(String("img"+i)).title;
+    nextIndex = document.getElementById(String("img"+i)).id;
+
+    zoomTest(nextSrc,nextTit,nextIndex);
+  }
+
+
+// function resetButtons(){
+//     document.getElementById("prev").disabled = false;
+//     document.getElementById("nxt").disabled = false;
+// }
